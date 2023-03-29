d6f596c05122e4a5a92ad26407cc795b7c9afc43
derek <derek@ubuntu.myguest.virtualbox.org>
2023-03-28 21:33:05 -0400

    Edited Register, created DeleteExercise, DeleteWorkout, SearchExercise, SearchWorkout

--- api.js
+++ api.js
@@ -18,39 +18,37 @@ exports.setApp = function (app, client){
     });
 
     // Login API
-    app.post('/api/login', async (req, res, next) => 
-    {
-    // incoming: login, password
-    // outgoing: id, firstName, lastName, error
-        
-    var error = '';
+    app.post('/api/login', async (req, res, next) => {
+        // incoming: login, password
+        // outgoing: id, firstName, lastName, error
+           
+        var error = '';
 
-    const { login, password } = req.body;
+        const { login, password } = req.body;
 
-    // Establish connection to database and await info from it.
-    const db = client.db("LargeProject");
-    const results = await db.collection('userInfo').find({login:login,password:password}).toArray();
+        // Establish connection to database and await info from it.
+        const db = client.db("LargeProject");
+        const results = await db.collection('userInfo').find({login:login,password:password}).toArray();
 
-    let id = -1;
-    let n = ''; // fill this with name from db.
-    let email = '';
-    let age = '';
-    let height = '';
-    let weight = '';
+        let id = -1;
+        let n = ''; // fill this with name from db.
+        let email = '';
+        let age = '';
+        let height = '';
+        let weight = '';
+    
+        if( results.length > 0 ) {
+            id = results[0]._id;  // case-sensitive to _id field on userInfo document
+            n = results[0].name;  // case-senstive to name field on userInfo document
+            email = results[0].email; // case-senstive to email field on userInfo document
+            age = results[0].age; // case-senstive to age field on userInfo document
+            height = results[0].height; // case-senstive to height field on userInfo document
+            weight = results[0].weight; // case-senstive to weight field on userInfo document
+        }
 
-    if( results.length > 0 )
-    {
-        id = results[0]._id;  // case-sensitive to _id field on userInfo document
-        n = results[0].name;  // case-senstive to name field on userInfo document
-        email = results[0].email; // case-senstive to email field on userInfo document
-        age = results[0].age; // case-senstive to age field on userInfo document
-        height = results[0].height; // case-senstive to height field on userInfo document
-        weight = results[0].weight; // case-senstive to weight field on userInfo document
-    }
-
-    // return json object containing user info
-    var ret = {id: id, name:n, email: email, age: age, height: height, weight: weight, error:error};
-    res.status(200).json(ret);
+        // return json object containing user info
+        var ret = {id: id, name:n, email: email, age: age, height: height, weight: weight, error:error};
+        res.status(200).json(ret);
     });
 
     // *******************
@@ -58,40 +56,47 @@ exports.setApp = function (app, client){
     // *******************
 
     // Register API
-    app.post('/api/register', async (req, res, next) => 
-    {
-    // incoming: name, login, password, email
-    // outgoing: error
+    app.post('/api/register', async (req, res, next) => {
+        // incoming: name, login, password, email
+        // outgoing: error
         
-    var error = '';
+        var error = '';
 
-    const { login, password, name, email } = req.body;
+        const { login, password, name, email } = req.body;
 
-    // create newUser object
-    // age, height, weight, and hasExercises are left blank for the addUserInfo api to fill later.
-    const newUser = {
-        login: login, 
-        password: password, 
-        name: name, 
-        email: email, 
-        age: null, 
-        height: null, 
-        weight: null, 
-        hasExercises: null, 
-        validated: false
-    };
+        // create newUser object
+        // age, height, weight, and hasExercises are left blank for the addUserInfo api to fill later.
+        const newUser = {
+            login: login, 
+            password: password, 
+            name: name, 
+            email: email, 
+            age: null, 
+            height: null, 
+            weight: null, 
+            hasExercises: null, 
+            validated: false
+        };
 
-    try{
         const db = client.db("LargeProject");
-        const results = await db.collection('userInfo').insertOne(newUser);
-    }
-    catch(e){
-        // set error message to error from DB if that point fails.
-        error = e.toString()
-    }
+        const results = await db.collection('userInfo').find({login:login}).toArray();
 
-    var ret = {error:error};
-    res.status(200).json(ret);  // return with HTML code 200 and error message json
+        try{
+
+            if(results.length > 0) {
+                throw "Username Taken";
+            } else if (login.toLowerCase() == "public") {
+                throw "Banned Name";
+            } else {
+                const results = await db.collection('userInfo').insertOne(newUser);
+            }
+        } catch(e) {
+            // set error message to error from DB if that point fails.
+            error = e.toString()
+        }
+
+        var ret = {error:error};
+        res.status(200).json(ret);  // return with HTML code 200 and error message json
     });
     
     // *******************
@@ -100,8 +105,7 @@ exports.setApp = function (app, client){
     
     // addUserInfo API
     // When the user first logs into their account they are prompted to input their age, weight, and height.
-	app.post('/api/addUserInfo', async (req, res, next) => 
-    {
+	app.post('/api/addUserInfo', async (req, res, next) => {
 		// incoming: age, weight, height, login (to search for the user in the database)
 		// outgoing: error message
 		
@@ -115,14 +119,8 @@ exports.setApp = function (app, client){
         // Try to find and update a user given a login field and 
         // update with the given age, height, and weight parameters.
         try{
-            result = await db.collection('userInfo').updateOne(
-                {"login" : login},  
-                {$set: {
-                    "age" : age, "height": height, "weight" : weight
-                }}
-            );
-        }
-        catch(e){
+            result = await db.collection('userInfo').updateOne({"login" : login}, {$set: {"age" : age, "height": height, "weight" : weight}});
+        } catch(e) {
             error = e.toString();
             // return error code 400, bad request.
             res.status(400).json({error: error});
@@ -142,8 +140,7 @@ exports.setApp = function (app, client){
     // End of addUserInfo API
     // **********************
 
-    app.post('/api/createWorkoutTemplate', async (req, res, next) => 
-    {
+    app.post('/api/createWorkoutTemplate', async (req, res, next) => {
 		// incoming: age, weight, height, login (to search for the user in the database)
 		// outgoing: error message
 		
@@ -162,8 +159,7 @@ exports.setApp = function (app, client){
         // update with the given age, height, and weight parameters.
         try{
             const result = await db.collection('workoutInfo').insertOne();
-        }
-        catch(e){
+        } catch(e) {
             error = e.toString();
             // return error code 400, bad request.
             res.status(400).json({error: error});
@@ -177,39 +173,173 @@ exports.setApp = function (app, client){
     // End of createWorkoutTemplate API
     // ********************************
 	
-    app.post('/api/getExercises', async (req, res, next) => 
-    {
-	// incoming: none
-	// outgoing: an array containing the user's exercises
-	var error = '';
-	const db = client.db("LargeProject");
-	// return all results but exclude the fields that aren't the exercises array
+    app.post('/api/getExercises', async (req, res, next) => {
+    	// incoming: none
+	    // outgoing: an array containing the user's exercises
+	
+        var error = '';
+	    const db = client.db("LargeProject");
+	
+        // return all results but exclude the fields that aren't the exercises array
         const results = await db.collection('workoutInfo').find({},{_id:0},{name:0},{date:0},{duration:0},{calories_burned:0}).toArray();
 		
-	var ret = {error:error};
+	    var ret = {error:error};
         res.status(200).json(ret);
-    }
-	     
-    // ***********************
-    // End of getExercises API
-    // ***********************
-	
-    app.post('/api/deleteWorkoutTemplate', async (req, res, next) => 
-    {
-	// incoming: workout id
-	// outgoing: nothing?
-		
-	const { id } = req.body;
-	var error = '';
-		
-	const db = client.db("LargeProject");
-        await db.collection('workoutInfo').deleteOne({_id:id});
-		
-	var ret = {error:error};
-        res.status(200).json(ret);
-    }
-	
+    });
+
     // ********************************
-    // End of deleteWorkoutTemplate API
-    // ********************************	     
+    // End of getExercises API
+    // ********************************
+
+    app.post('/api/searchExercise', async(req, res, next) => {
+        // incoming: login
+        // outgoing: all exercises this user has
+
+        var error = '';
+
+        const { userName } = req.body;
+
+        const db = client.db("LargeProject");
+
+        const results = await db.collection('userInfo').find({login: userName}).toArray();
+
+        try {
+            if(results.length > 0) {
+                var searchPublic = results[0].login;
+            } else {
+                throw "No Such User";
+            }
+
+            const result = await db.collection('exerciseInfo').find({public: searchPublic}).toArray();
+        
+            result.push(await db.collection('exerciseInfo').find({public: "public"}).toArray());
+
+            var ret = {exercises: result};
+            res.status(200).json(ret);
+        } catch(e) {
+            // set error message to error from DB if that point fails.
+            error = e.toString();
+
+            // return error code 404, User not found
+            var ret = {error:error};
+            res.status(404).json(ret);
+        }
+    });
+
+    // ********************************
+    // End of searchExercise API
+    // ********************************
+
+    app.post('/api/searchWorkout', async(req, res, next) => {
+        // incoming: login
+        // outgoing: all exercises this user has
+
+        var error = '';
+
+        const { userName } = req.body;
+
+        const db = client.db("LargeProject");
+
+        const results = await db.collection('userInfo').find({login: userName}).toArray();
+
+        try {
+            if(results.length > 0) {
+                var searchPublic = results[0].login;
+            } else {
+                throw "No Such User";
+            }
+
+            const result = await db.collection('workoutInfo').find({public: searchPublic}).toArray();
+        
+            result.push(await db.collection('workoutInfo').find({public: "public"}).toArray());
+
+            var ret = {workouts: result};
+            res.status(200).json(ret);
+        } catch(e) {
+            // set error message to error from DB if that point fails.
+            error = e.toString();
+
+            // return error code 404, User not found
+            var ret = {error:error};
+            res.status(404).json(ret);
+        }
+    });
+
+    // ********************************
+    // End of searchWorkout API
+    // ********************************
+
+    app.post('/api/deleteExercise', async(req, res, next) => {
+        // incoming: exercise name, login
+        // outgoing: none
+
+        var error = '';
+
+        const { eName, userName } = req.body;
+
+        const db = client.db("LargeProject");
+
+        const results = await db.collection('userInfo').find({login: userName}).toArray();
+
+        try {
+            if(results.length > 0) {
+                var searchPublic = results[0].login;
+            } else {
+                throw "No Such User";
+            }
+
+            const result = await db.collection('exerciseInfo').deleteMany({name: eName, public: userName});            
+        } catch(e) {
+            // set error message to error from DB if that point fails.
+            error = e.toString();
+
+            // return error code 404, User not found
+            var ret = {error:error};
+            res.status(404).json(ret);
+        }
+
+        var ret = {error:error};
+        res.status(200).json(ret);
+    });
+
+    // ********************************
+    // End of deleteExercise API
+    // ********************************
+
+    app.post('/api/deleteWorkout', async(req, res, next) => {
+        // incoming: workout name, login
+        // outgoing: none
+
+        var error = '';
+
+        const { wName, userName } = req.body;
+
+        const db = client.db("LargeProject");
+
+        const results = await db.collection('userInfo').find({login: userName}).toArray();
+
+        try {
+            if(results.length > 0) {
+                var searchPublic = results[0].login;
+            } else {
+                throw "No Such User";
+            }
+
+            const result = await db.collection('workoutInfo').deleteMany({name: wName, public: userName});      
+            
+            var ret = {error:error};
+            res.status(200).json(ret);
+        } catch(e) {
+            // set error message to error from DB if that point fails.
+            error = e.toString();
+
+            // return error code 404, User not found
+            var ret = {error:error};
+            res.status(404).json(ret);
+        }
+    });
+
+    // ********************************
+    // End of deleteWorkout API
+    // ********************************
 }
