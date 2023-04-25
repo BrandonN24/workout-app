5a8b0d9e4c12d5b3676871642563f56e1a1bfe8c
Brandon Nguyen <brandonn24@knights.ucf.edu>
2023-04-25 12:26:23 -0400

    Added bootstrap JS and CSS

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -57,6 +57,8 @@ function Login()
                 let sendID = {id: decodedToken.id , jwtToken: storage.retrieveToken()};
                 let jsIdObj = JSON.stringify(sendID);
 
+                console.log(jsIdObj);
+                
                 // make a call to get userInfo
                 const infoRequest = await fetch(bp.buildPath('api/getUserInfo'),
                 {method:'POST',body:jsIdObj,headers:{'Content-Type': 'application/json'}});
--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -3,7 +3,11 @@ import React from 'react';
 function PageTitle()
 {
    return(
-     <h1 id="title">Workout App</h1>
+    <body>
+      <h1 id="title">UCFitness App</h1>
+    </body>
+     
+    
    );
 };
 
