1c129bbef1a1ba0aad0181f870b28cced1883df6
Mateo Gamero <gameromateo@knights.ucf.edu>
2023-04-29 19:06:56 -0400

    Profile

--- workout_mobile/components/screens/Profile.js
+++ workout_mobile/components/screens/Profile.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import jwtDecode from 'jwt-decode';
 
-const ProfileScreen = ({}) => {
+const ProfileScreen = () => {
   const navigation = useNavigation();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        const decodedToken = jwtDecode(token);
+        const data = await AsyncStorage.getItem('data');
+        const parsedData = JSON.parse(data);
+        setName(decodedToken.name);
+        setEmail(parsedData.email);
+        setAge(parsedData.age);
+        setHeight(parsedData.height);
+        setWeight(parsedData.weight);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getToken();
+  }, []);
+
   const handleSignOut = () => {
     navigation.navigate('Login');
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to your profile!</Text>
+      <Text style={styles.title}>Welcome to your profile, {name}!</Text>
+      <Text style={styles.subtitle}>Email: {email}</Text>
+      <Text style={styles.subtitle}>Age: {age}</Text>
+      <Text style={styles.subtitle}>Height: {height} ft</Text>
+      <Text style={styles.subtitle}>Weight: {weight} lb</Text>
       <View style={styles.buttonContainer}>
         <Button title="Sign Out" onPress={handleSignOut} color="#3C6E71" />
       </View>
@@ -29,20 +60,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 50,
   },
-  button: {
-    position: 'absolute',
-    bottom: 0,
-    marginBottom: 20,
-    color: '#3C6E71',
+  subtitle: {
+    fontSize: 18,
+    marginTop: 10,
   },
   buttonContainer: {
-    position: 'absolute', // Added to position the container absolutely
-    bottom: 0, // Positioned at the bottom of the screen
-    left: 0, // Positioned at the left of the screen
-    right: 0, // Positioned at the right of the screen
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
     alignItems: 'center',
-    paddingTop: 50, // Added padding to push the Text component up
-    paddingBottom: 50, // Added padding to push the Button component up
+    paddingTop: 50,
+    paddingBottom: 50,
   },
 });
 
